e9e0ae85d800c401ace034e0fd7bfa47ecf9b3b1
noisyfox <timemanager.rick@gmail.com>
2020-05-07 19:58:22 +1000

    Add DPI Utils from gitextensions

---
+++ ACT.FoxCommon/dpi/ControlDpiExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ACT.FoxCommon.dpi
+{
+    /// <summary>
+    /// https://github.com/gitextensions/gitextensions/blob/d01541bf329f6d601ddd30f23e7f1d83462ebb67/GitExtUtils/GitUI/ControlDpiExtensions.cs
+    /// </summary>
+    public static class ControlDpiExtensions
+    {
+        public static void AdjustForDpiScaling(this Control control)
+        {
+            if (control == null)
+            {
+                throw new ArgumentNullException(nameof(control));
+            }
+
+            var isDpiScaled = DpiUtil.IsNonStandard;
+
+            // If we are in design mode, don't scale anything as the designer may
+            // write scaled values back to InitializeComponent.
+            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+            {
+                return;
+            }
+
+            foreach (var descendant in control.FindDescendants())
+            {
+                // NOTE we can't automatically scale TreeView or ListView here as
+                // adjustment must be done before images are added to the
+                // ImageList otherwise they're all removed.
+                switch (descendant)
+                {
+                    case ButtonBase button:
+                        {
+                            if (isDpiScaled && button.Image != null)
+                            {
+                                button.Image = DpiUtil.Scale(button.Image);
+                                button.Padding = DpiUtil.Scale(new Padding(4, 0, 4, 0));
+                            }
+
+                            break;
+                        }
+
+                    case PictureBox pictureBox:
+                        {
+                            if (isDpiScaled && pictureBox.Image != null)
+                            {
+                                pictureBox.Image = DpiUtil.Scale(pictureBox.Image);
+                            }
+
+                            break;
+                        }
+
+                    // case TabControl tabControl:
+                    //     {
+                    //         if (!isDpiScaled)
+                    //         {
+                    //             tabControl.Padding = new Point(8, 6);
+                    //         }
+                    //         else if (tabControl.Tag as string != "__DPI_SCALED__")
+                    //         {
+                    //             tabControl.Tag = "__DPI_SCALED__";
+                    //             tabControl.Padding = DpiUtil.Scale(new Point(8, 6));
+                    //         }
+                    //
+                    //         break;
+                    //     }
+
+                    // case SplitContainer splitContainer:
+                    //     {
+                    //         const int splitterWidth = 8;
+                    //
+                    //         if (!isDpiScaled)
+                    //         {
+                    //             splitContainer.SplitterWidth = splitterWidth;
+                    //         }
+                    //         else if (splitContainer.Tag as string != "__DPI_SCALED__")
+                    //         {
+                    //             splitContainer.Tag = "__DPI_SCALED__";
+                    //             splitContainer.SplitterWidth = DpiUtil.Scale(splitterWidth);
+                    //         }
+                    //
+                    //         splitContainer.BackColor = ColorHelper.GetSplitterColor();
+                    //         break;
+                    //     }
+
+                    case TextBoxBase textBox when textBox.Margin == new Padding(12):
+                        {
+                            // Work around a bug in WinForms where the control's margin gets scaled beyond expectations
+                            // see https://github.com/gitextensions/gitextensions/issues/5098
+                            textBox.Margin = DpiUtil.Scale(new Padding(3));
+                            break;
+                        }
+
+                    case UpDownBase upDown when upDown.Margin == new Padding(96):
+                        {
+                            // Work around a bug in WinForms where the control's margin gets scaled beyond expectations
+                            // see https://github.com/gitextensions/gitextensions/issues/5098
+                            upDown.Margin = DpiUtil.Scale(new Padding(3));
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ ACT.FoxCommon/dpi/ControlUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ACT.FoxCommon.dpi
+{
+    /// <summary>
+    /// https://github.com/gitextensions/gitextensions/blob/4695bb371563922d43cccfbbc3b79d6b704096d2/GitExtUtils/GitUI/ControlUtil.cs
+    /// </summary>
+    public static class ControlUtil
+    {
+        /// <summary>
+        /// Enumerates all descendant controls.
+        /// </summary>
+        public static IEnumerable<Control> FindDescendants(this Control control,
+            Func<Control, bool> skip = null)
+        {
+            var queue = new Queue<Control>();
+
+            foreach (Control child in control.Controls)
+            {
+                if (skip?.Invoke(control) == true)
+                {
+                    continue;
+                }
+
+                queue.Enqueue(child);
+            }
+
+            while (queue.Count != 0)
+            {
+                var c = queue.Dequeue();
+
+                yield return c;
+
+                foreach (Control child in c.Controls)
+                {
+                    if (skip?.Invoke(child) == true)
+                    {
+                        continue;
+                    }
+
+                    queue.Enqueue(child);
+                }
+            }
+        }
+    }
+}
