84c12d3f640e063e0474e1894a9657c0863c6451
noisyfox <timemanager.rick@gmail.com>
2022-04-18 12:03:48 +0800

    Update copyright

--- ACT.FoxCommon/Properties/AssemblyInfo.cs
+++ ACT.FoxCommon/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ACT.FoxCommon")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018-2022 Noisyfox")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
